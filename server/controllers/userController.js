0264d01d25c67174086af74ca1631e18718a44af
nimit sodhani <nimitsodhani1@gmail.com>
2024-05-17 17:48:15 +0530

    login func created

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -60,6 +60,30 @@ const userCtr = {
         return res.status(500).json({msg : err.message});
       }
      
+    },
+    login  : async ( req , res  )=>{
+       try {
+        const {email , password} = req.body;
+        const user = await Users.findOne({email});
+        if(!user) return res.status(400).json({msg : "User does not exist."});
+        
+        const isMatch = await bcrypt.compare(password , user.password);
+        if(!isMatch) return res.status(400).json({msg : "Incorrect password."});
+        const accesstoken = createAccessToken({id : user._id});
+        const refreshtoken = createRefreshToken({id : user._id});
+
+        res.cookie('refreshtoken' , refreshtoken , {
+          httpOnly : true ,
+          path : '/user/refreshtoken'
+        })
+        res.json({msg : "login success!"});
+
+
+       }
+        catch(err) {
+          return res.status(500).json({msg : err.message});
+
+        }
     }
   }
 const createAccessToken=(payload)=>{
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,4 +3,5 @@ const router = require('express').Router();
 const userCtr = require('../controllers/userController');
 router.post('/register', userCtr.register);
 router.post('/refreshtoken', userCtr.refreshtoken);
+router.post('/login'  , userCtr.login);
 module.exports = router;
